1f9d236a3ae665ae5a3e34e62df0eb4056ee932a
Demon-mark <63382030+Demon-mark@users.noreply.github.com>
2020-06-19 15:55:51 +0800

    Add files via upload
    A simple implements of single linked list's 2 genneration

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -1,6 +1,6 @@
 /**
  *
- * 简单元素不可重复单链表的实现
+ * 简单单链表的实现
  * 利用泛型实现各种对象的添加(add addAll)、删除(remove, removeAll)、查找(search)、更改(modify)操作
  *
  * @author Amosen
@@ -94,6 +94,114 @@ public class SingleLinkedList<T> {
 
     public void clear() {
         head = new Node(null);
+    }
+
+    public int getLength() {
+        Node temp = head.next;
+        int length = 0;
+        while(temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        return length;
+    }
+
+    private Node searchPositive(int index) {
+        Node temp = head.next;
+        int value = 0;
+        while(temp != null) {
+            value++;
+            if(value == index) {
+                break;
+            }
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    private Node searchNegative(int index) {
+        Node tempAhead = head.next;
+        Node tempBehind = head.next;
+
+        int value = 0;
+        while(tempAhead != null) {
+            value++;
+            tempAhead = tempAhead.next;
+            if(value == -index + 2) {
+                value--;
+                tempBehind = tempBehind.next;
+            }
+        }
+        return tempBehind;
+    }
+
+    public T search(int index) {
+
+        if(getLength() == 0) {
+            return null;
+        }
+
+        if(Math.abs(index) > getLength()) {
+            return null;
+        }
+
+        if(index > 0 && index > getLength() - 1) {
+            return null;
+        }
+
+        if(index >= 0) {
+            Node pn = searchPositive(index);
+            return (T) pn.next.getValue();
+        }
+
+        if(index < 0) {
+            Node nn = searchNegative(index);
+            return (T) nn.next.getValue();
+        }
+        return null;
+    }
+
+    public boolean insert(int index, T value) {
+        if(index > getLength() - 1 || Math.abs(index) > getLength()) {
+            throw new IndexOutOfBoundsException("The List doesn't has the index of " + index);
+        }
+
+        if(search(value) != null) {
+            return false;
+        }
+
+        Node insertNode = swap(value);
+        if(index >= 0) {
+            Node np = searchPositive(index);
+            insertNode.next = np.next;
+            np.next = insertNode;
+            return true;
+        }
+
+        if(index < 0) {
+            Node nn = searchNegative(index);
+            insertNode.next = nn.next;
+            nn.next = insertNode;
+            return true;
+        }
+        return false;
+    }
+
+    public void reverse() {
+        if(getLength() == 0 || getLength() == 1) {
+            return;
+        }
+        Node reverseNode = new Node(null);
+        Node temp = head.next;
+        Node cur = reverseNode.next;
+        while(temp != null) {
+            head.next = temp.next;
+            temp.next = cur;
+            cur = temp;
+            reverseNode.next = cur;
+            temp = head.next;
+        }
+        head.next = reverseNode.next;
     }
 
     public void show() {
